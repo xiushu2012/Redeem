c962a33a8f36d0ad98bc708a862738ae08f3d78e
xiushu2012 <zhbing2006@163.com>
2023-10-07 10:27:52 +0800

    first commit

---
+++ crawler.py
@@ -0,0 +1,92 @@
+﻿# -*- coding: utf-8 -*-
+	
+import pandas as pd
+from selenium import webdriver
+import time
+import json
+import datetime
+import os
+
+def get_browser(url):
+    options = webdriver.ChromeOptions()
+    #options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    options.add_experimental_option('excludeSwitches', ['enable-automation'])
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    driver = webdriver.Chrome(options=options)
+
+
+    driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+        "source": """
+                        Object.defineProperty(navigator, 'webdriver', {
+                          get: () => undefined
+                        })
+                      """
+    })
+    
+    get_cookie(driver,url)
+    set_cookie(driver)
+    
+    return driver
+
+
+
+def set_cookie(driver):
+	with open('cookies.json', 'r', encoding='utf-8') as f:
+		listCookies = json.loads(f.read())
+
+	for cookie in listCookies:
+		driver.add_cookie(cookie)
+
+
+def get_cookie(driver,url):
+	driver.get(url)
+	#这里可以手动登录 或者代码登录 
+	time.sleep(50)
+	dictCookies = driver.get_cookies()
+	jsonCookies = json.dumps(dictCookies)
+	print(jsonCookies)
+	# 登录完成后，将cookie保存到本地文件
+	with open('cookies.json', 'w') as f:
+		f.write(jsonCookies)
+	print("please check cookie in cookies.json")
+
+def convert_float(rt1,rt2):
+		ratio1 = float(rt1.strip('%'))
+		ratio2 = float(rt2.strip('%'))
+		return ratio1,ratio2
+
+
+if __name__=='__main__':
+			url = 'https://www.jisilu.cn/web/data/cb/delisted'
+			browser = get_browser(url)
+						
+			browser.get(url)
+			time.sleep(10)
+			data = browser.page_source
+			tables = pd.read_html(data,header=None)
+			print(f"tables[0]:{tables[0]}")
+			print(f"tables[1]:{tables[1]}")
+			
+			
+			column1 = tables[1].columns.tolist()
+			column0 = tables[0].columns.tolist()
+			zipped = zip(column1, column0)
+			#使用dict()函数将元组列表转换为字典
+			dictionary = dict(zipped)
+			print(f"column1:{column1},column0:{column0},dictionary:{dictionary}")
+			jsl_df_raw = tables[1].rename(columns=dictionary)
+			jsl_df = jsl_df_raw.rename(columns={'发行规模(亿元)': '发行规模', '回售规模(亿元)': '回售规模','剩余规模(亿元)': '剩余规模','存续年限(年)': '存续年限'})
+
+			print(f"jsl_df:{jsl_df}")
+			
+			tnow = datetime.datetime.now()
+			print("time is :" + tnow.strftime('%Y%m%d'))
+			filein = tnow.strftime('%Y_%m_%d') + '_in.xlsx'
+			getakpath =  "./%s" % (filein)
+			jsl_df.replace('-','0',inplace=True)
+			
+			#修改名称
+			
+			#jsl_df.to_excel(getakpath,header=['代码','名称','最后交易价格', '正股代码','正股名称', '发行规模','回售规模','剩余规模', '发行日期','最后交易日', '到期日期','存续年限','退市原因'],index=False,sheet_name='jsl')
+			jsl_df.to_excel(getakpath,index=False,sheet_name='jsl')
+			print("data of path:" + getakpath)
---
+++ redeem.py
@@ -0,0 +1,58 @@
+﻿# -*- coding: utf-8 -*-
+
+
+
+import akshare as ak
+import numpy as np  
+import pandas as pd  
+import math
+import datetime
+import os
+import matplotlib.pyplot as plt
+import openpyxl
+import re
+
+if __name__=='__main__':
+
+    from sys import argv
+    filein = ""
+    if len(argv) > 1:
+        filein = argv[1]
+    else:
+        print("please run like 'python redeem.py [file]'")
+        exit(1)
+
+    quit_jsl_df = pd.read_excel(filein,'jsl')
+    quit_jsl_df = quit_jsl_df[~quit_jsl_df['名称'].str.endswith('EB')]
+    
+    
+    reason_jsl_count = quit_jsl_df['退市原因'].count()
+    reason_redeem_count = quit_jsl_df['退市原因'].value_counts()['强赎']
+    
+    redeem_ratio = reason_redeem_count/reason_jsl_count
+    print("The ratio of redeem:%f,Total Count:%d,Redeem Count:%d" % (redeem_ratio,reason_jsl_count,reason_redeem_count))
+    
+    print("======强赎条件下的存续年限分布======")
+    redeem_jsl_df = quit_jsl_df[quit_jsl_df['退市原因'] == '强赎']
+    print(redeem_jsl_df['存续年限'].describe(percentiles = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.91,0.92,0.93,0.94,0.95,0.96,0.97,0.98,0.99]))
+
+    redeem_jsl_df = quit_jsl_df[quit_jsl_df['退市原因'] != '强赎']
+    print("======非强赎条件下的存续年限分布======")
+    print(redeem_jsl_df['存续年限'].describe(percentiles = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]))
+
+
+    redeem_small_jsl_df = quit_jsl_df[quit_jsl_df['发行规模'] <=5.0 ]
+    print("======小于5亿的剩余转债最后交易价格限分布======")
+    print(redeem_small_jsl_df['最后交易价格'].describe(percentiles = [0.2,0.4,0.5,0.6,0.8]))
+    
+    fileout = filein.replace('in','out')
+    writer = pd.ExcelWriter(fileout)
+    redeem_small_jsl_df.to_excel(writer,'redeem')
+    writer.save()
+    print("out put redeem statistics time:" + fileout)
+
+
+
+
+
+
